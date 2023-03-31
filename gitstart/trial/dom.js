12727d225b0a42e6f27342134438bc3f2a54168e
krushnasharma05 <krushnasharma05@gmail.com>
2023-03-31 23:44:15 +0530

    dom

---
+++ gitstart/trial/dom.js
@@ -0,0 +1,10 @@
+//console.dir(document);
+console.log(document.title);
+document.title='hello'; 
+
+var headertitle=  document.getElementById('header-title');
+var header=  document.getElementById('main-header');
+header.style.borderBottom='solid 2px #000'; 
+var title=  document.getElementById('main');
+title.style.color='green'; 
+title.style.fontWeight = 'bold';
