fb8c18d2391e1dad4ed06fd787b03b46ae4ff128
krushnasharma05 <krushnasharma05@gmail.com>
2023-04-01 10:14:09 +0530

    getbytag

--- gitstart/filee.js
+++ gitstart/filee.js
@@ -1,6 +1,14 @@
-var items = document.getElementsByClassName('list-group-item');
-console.log(items[2]);
-items[2].style.backgroundColor = 'green';
-for (var i= 0;i < items.length; i++){
-    items[i].style.fontWeight='bold';
-}
+// var items = document.getElementsByClassName('list-group-item');
+// console.log(items[2]);
+// items[2].style.backgroundColor = 'green';
+// for (var i= 0;i < items.length; i++){
+//     items[i].style.fontWeight='bold';
+// }
+
+ var it = document.getElementsByClassName('list-item');
+ console.log(it);
+ it[0].style.fontWeight= 'bold';
+ var it = document.getElementsByTagName('li2');
+ console.log(it);
+ it[0].style.color='green';
+ 
